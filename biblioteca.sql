90051d9ea4af5a0963684b25b3f73af8b8df1e6b
Pablo Antuña <pablo.antuna99@gmail.com>
2021-10-28 21:27:30 -0300

    falta apartado e del ejercicio 5

--- biblioteca.sql
+++ biblioteca.sql
@@ -45,19 +45,30 @@ CREATE INDEX titulo_idx ON Libro(titulo);
 /* Ejercicio 3 */
 
 /* En la tabla Autor */
-INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('Abelardo', 'Castillo', 'Argentino', 'San Pedro');
-/*INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('', '', '', '');
-INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('', '', '', '');*/
+INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('Abelardo', 'Castillo', 'argentina', 'San Pedro');
+INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('Jane', 'Austen', 'inglesa', 'Steventon');
+INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('Jorge Luis', 'Borges', 'argentina', 'Buenos Aires');
+INSERT INTO Autor (nombre, apellido, nacionalidad, residencia) VALUES ('Ernest', 'Hemingway', 'estadounidense', 'Illinois');
 
 /* En la tabla Libro */
 INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789500411110', 'Cronica de un iniciado', 'UNR', 100);
-/*INSERT INTO Libro;
-INSERT INTO Libro;*/
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789507312267', 'El evangelio segun Van Hutten', 'Planeta', 200);
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789500803359', 'Orgullo y Prejuicio', 'Atlantida', 250);
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789870408543', 'Emma', 'UNR', 150);
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789500401906', 'El libro de arena', 'UNR', 169);
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789500416276', 'Elogio de la sombra', 'Emece', 142);
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789507428739', 'El viejo y el mar', 'Planeta', 120);
+INSERT INTO Libro (isbn, titulo, editorial, precio) VALUES ('9789503701386', 'Paris era una fiesta', 'Planeta', 300);
 
 /* En la tabla Escribe */
 INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Abelardo' AND apellido = 'Castillo'), '9789500411110', 1991);
-/*INSERT INTO Escribe;
-INSERT INTO Escribe;*/
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Abelardo' AND apellido = 'Castillo'), '9789507312267', 1998);
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Jane' AND apellido = 'Austen'), '9789500803359', 1814);
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Jane' AND apellido = 'Austen'), '9789870408543', 1815);
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Jorge Luis' AND apellido = 'Borges'), '9789500401906', 1998);
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Jorge Luis' AND apellido = 'Borges'), '9789500416276', 1985);
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Ernest' AND apellido = 'Hemingway'), '9789507428739', 1998);
+INSERT INTO Escribe (id, isbn, ano) VALUES ((SELECT id FROM Autor WHERE nombre = 'Ernest' AND apellido = 'Hemingway'), '9789503701386', 1983);
 
 /* Ejercicio 4 */
 
@@ -67,10 +78,17 @@ SET residencia = 'Buenos Aires'
 WHERE nombre = 'Abelardo'
 AND apellido = 'Castillo';
 /* Apartado b */
-UPDATE Libro l1
+UPDATE Libro
 SET precio = 1.1 * precio
 WHERE editorial = 'UNR';
 /* Apartado c */
-/*UPDATE ;*/
+UPDATE Libro
+SET precio = 1.1 * precio
+WHERE (SELECT nacionalidad FROM Autor WHERE Autor.id = (SELECT Escribe.id FROM Escribe WHERE Escribe.isbn = Libro.isbn)) <> 'argentina' AND precio > 200;
+
+UPDATE Libro
+SET precio = 1.2 * precio
+WHERE (SELECT nacionalidad FROM Autor WHERE Autor.id = (SELECT Escribe.id FROM Escribe WHERE Escribe.isbn = Libro.isbn)) <> 'argentina' AND precio <= 200
 /* Apartado d */
-/*DELETE ;*/
+DELETE FROM Libro
+WHERE ano = 1998;
--- consultas.sql
+++ consultas.sql
@@ -7,13 +7,35 @@ Garavano Lautaro - Legajo: G-5582/4
 /* Ejercicio 5 */
 
 /* Apartado a */
-
+SELECT nombre FROM Persona WHERE
+codigo IN (SELECT codigo FROM Propietario);
 /* Apartado b */
+SELECT codigo FROM Inmueble WHERE precio >= 600000 AND precio <= 700000;
 
 /* Apartado c */
+SELECT nombre FROM Persona 
+WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona 
+                 WHERE nombre_zona = 'Norte' 
+                 AND nombre_poblacion = 'Santa Fe')
+GROUP BY nombre 
+HAVING COUNT(nombre) = (SELECT COUNT(codigo_cliente) FROM PrefiereZona 
+                        WHERE codigo_cliente = codigo);
 
 /* Apartado d */
+SELECT nombre FROM Persona WHERE
+codigo IN (SELECT vendedor FROM Cliente WHERE
+EXISTS (SELECT * FROM PrefiereZona WHERE
+        codigo_cliente = Cliente.codigo 
+        AND nombre_poblacion = 'Rosario' 
+        AND nombre_zona = 'Centro'));
 
 /* Apartado e */
+SELECT
 
 /* Apartado f */
+SELECT nombre FROM Persona 
+WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona 
+                 WHERE nombre_poblacion = 'Santa Fe')
+GROUP BY nombre 
+HAVING COUNT(nombre) = (SELECT COUNT(*) FROM Zona
+                        WHERE nombre_poblacion = 'Santa Fe');
