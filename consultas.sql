05319430cde663ed82e9adef5cb1fcefc425e922
Pablo Antuña <pablo.antuna99@gmail.com>
2021-10-31 18:46:39 -0300

    terminadas consultas ej5

--- consultas.sql
+++ consultas.sql
@@ -13,13 +13,12 @@ codigo IN (SELECT codigo FROM Propietario);
 SELECT codigo FROM Inmueble WHERE precio >= 600000 AND precio <= 700000;
 
 /* Apartado c */
-SELECT nombre FROM Persona 
-WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona 
-                 WHERE nombre_zona = 'Norte' 
-                 AND nombre_poblacion = 'Santa Fe')
-GROUP BY nombre 
-HAVING COUNT(nombre) = (SELECT COUNT(codigo_cliente) FROM PrefiereZona 
-                        WHERE codigo_cliente = codigo);
+SELECT nombre FROM Persona
+WHERE Persona.codigo IN
+    (SELECT Cliente.codigo FROM Cliente
+     WHERE Cliente.codigo IN (SELECT codigo_cliente FROM PrefiereZona) AND Cliente.codigo NOT IN
+        (SELECT codigo_cliente FROM PrefiereZona
+         WHERE NOT (nombre_poblacion = "Santa Fe" AND nombre_zona = "Norte")));
 
 /* Apartado d */
 SELECT nombre FROM Persona WHERE
@@ -30,12 +29,11 @@ EXISTS (SELECT * FROM PrefiereZona WHERE
         AND nombre_zona = 'Centro'));
 
 /* Apartado e */
-SELECT
+SELECT nombre_zona, COUNT(*), AVG(precio) FROM Inmueble WHERE nombre_poblacion = "Rosario" GROUP BY nombre_zona;
 
 /* Apartado f */
 SELECT nombre FROM Persona 
 WHERE codigo IN (SELECT codigo_cliente FROM PrefiereZona 
-                 WHERE nombre_poblacion = 'Santa Fe')
-GROUP BY nombre 
-HAVING COUNT(nombre) = (SELECT COUNT(*) FROM Zona
-                        WHERE nombre_poblacion = 'Santa Fe');
+                 WHERE nombre_poblacion = 'Santa Fe'
+                 GROUP BY codigo_cliente
+                 HAVING COUNT(nombre_poblacion) = (SELECT COUNT(*) FROM Zona WHERE nombre_poblacion = 'Santa Fe'));
